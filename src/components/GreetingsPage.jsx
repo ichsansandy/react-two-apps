bb5c3e6c3026ed529a7b0176e165a287252ba100
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-23 14:06:21 +0700

    Add redux for greetings

--- src/components/GreetingsPage.jsx
+++ src/components/GreetingsPage.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRandomGreeting } from '../redux/greetings/greetingsSlice';
 
 export default function GreetingsPage() {
-  return <div className="text-title">Adios amigo</div>;
+  const dispatch = useDispatch();
+  const greetings = useSelector((state) => state.greetings.value);
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  return <div className="text-title">{greetings}</div>;
 }
